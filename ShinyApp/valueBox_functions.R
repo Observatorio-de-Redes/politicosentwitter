7409022a546e2af95748adc8b1ab6f86c0b6af6a
Guadalupe Gonzalez <31290160+Guadag12@users.noreply.github.com>
2021-05-21 18:46:37 -0300

    Update valueBox_functions.R

--- ShinyApp/valueBox_functions.R
+++ ShinyApp/valueBox_functions.R
@@ -112,127 +112,125 @@ valueBox_ndi <- function() {
       )
   )
   
-}
-valueBox_about <- function(imagen_vb, icon1, icon2, web1, web2, text, width_, height_) {
-  div(class = "col-lg-3 col-md-10",
-      div(class = "panel panel-primary",
-          div(class = "panel-heading", style = paste0("background-color:#ffffff"),
-              div(class = "row",
-                  div(class = "col-xs-10", style = "align : center;",
-                      tags$a(tags$img(src=imagen_vb, height=width_,align = "center",width=height_,style="display: block; "),  href=web1)
-                  )
-                
-              )
-          ),
-          div(class = "panel-footer",
-              div(class = "clearfix",
-                  div(class = "row",
-                      fluidRow(div(class = "col-xs-6",
-                          column(4,tags$a(icon(icon1, "fa-2x"),href=web1))
-                      ),
-                      div(class = "col-xs-4",
-                          column(1,tags$a(icon(icon2, "fa-2x"),href=web2)))
-                  ))
-                      ))
-          )
-      )
-  
-}
-
-valueBox_about_oder <- function(imagen_vb, icon1, icon2, web1, web2, text, width_, height_) {
-  div(class = "col-lg-3 col-md-10",
-      div(class = "panel panel-primary",
-          div(class = "panel-heading", style = paste0("background-color:#ffffff"),
-              div(class = "row",
-                  div(class = "col-xs-10", style = "align: center;",
-                      column(4),
-                      tags$a(tags$img(src=imagen_vb, height=width_,align = "center",width=height_,
-                                      style="display: block; "),  href=web1)
-                  )
+}  valueBox_about <- function(imagen_vb, icon1, icon2, web1, web2, text, width_, height_) {
+    div(class = "col-lg-3 col-md-10",
+        div(class = "panel panel-primary",
+            div(class = "panel-heading", style = paste0("background-color:#ffffff"),
+                div(class = "row",
+                    div(class = "col-xs-10", style = "text-align: center;",
+                        tags$a(tags$img(src=imagen_vb, height=width_,align = "center",width=height_,style="display: block; text-align:right;"),  href=web1)
+                    ))
                   
-              )
-          ),
-          div(class = "panel-footer",
-              div(class = "clearfix",
-                  div(class = "row",
-                      fluidRow(div(class = "col-xs-6",
-                                   column(4,tags$a(icon(icon1, "fa-2x"),href=web1))
-                      ),
-                      div(class = "col-xs-4",
-                          column(1,tags$a(icon(icon2, "fa-2x"),href=web2)))
-                      ))
-              ))
-      )
-  )
+                ),
+            div(class = "panel-footer",
+                div(class = "clearfix",
+                    div(class = "row",
+                        fluidRow(div(class = "col-xs-6",
+                                     column(10),tags$a(icon(icon1, "fa-2x"),href=web1)
+                        ),
+                        div(class = "col-xs-4",
+                            column(1,tags$a(icon(icon2, "fa-2x"),href=web2)))
+                    ))
+                        ))
+            )
+    )
+    
+  }
   
-}
-
-valueBox_people1 <- function(imagen_vb, icon1, icon2, web1, web2, text, width_, height_,text2) {
-  div(class = "col-lg-3 col-md-11",
-      div(class = "panel panel-primary",
-          div(class = "panel-heading", style = paste0("background-color:#ffffff"),
-              div(class = "row",
-                  fluidRow(    
-                      column(4),
-                      div(class = "col-xs-6", style = "align: center;",
-                          
-                      tags$a(tags$img(src=imagen_vb, height=width_,align = "center",width=height_,
-                                      style="display: block; "),  href=web1)
-                  ), 
-                  div(class = ("col-xs-9 text-right"),
-                      div(style = "font-size: 25px; color: #15202b;font-weight: bold; text-align:center; ",
-                          column(5),h4(text, style = "text-align:center;font-weight: bold;"), 
-                          column(5),h6(text2, style= "color: #7a7a7a; text-align:center")
-                      )
-                  
-              ))
-          )),
-          div(class = "panel-footer",
-              div(class = "clearfix",
-                  div(class = "row",
-                      fluidRow(div(class = "col-xs-6",
-                                   column(4,tags$a(icon(icon1, "fa-2x"),href=web1))
-                      ),
-                      div(class = "col-xs-4",
-                          column(1,tags$a(icon(icon2, "fa-2x"),href=web2)))
-                      ))
-              ))
-      )
-  )
+  valueBox_about_oder <- function(imagen_vb, icon1, icon2, web1, web2, text, width_, height_) {
+    div(class = "col-lg-3 col-md-10",
+        div(class = "panel panel-primary",
+            div(class = "panel-heading", style = paste0("background-color:#ffffff"),
+                div(class = "row",
+                    div(class = "col-xs-10",
+                        tags$a( tags$img(src=imagen_vb, height=width_, width=height_,
+                                        style="display: block; float:right; 
+                                        right:50px;'"),  href=web1)
+                    )
+                    
+                )
+            ),
+            div(class = "panel-footer",
+                div(class = "clearfix",
+                    div(class = "row",
+                        fluidRow(div(class = "col-xs-6",
+                                     column(10),tags$a(icon(icon1, "fa-2x"),href=web1)
+                        ),
+                        div(class = "col-xs-4",
+                            column(1,tags$a(icon(icon2, "fa-2x"),href=web2)))
+                        ))
+                ))
+        )
+    )
+    
+  }
   
-}
-
-valueBox_people2 <- function(imagen_vb, icon1, icon2, web1, web2, text, width_, height_,text2) {
-  div(class = "col-lg-3 col-md-11",
-      div(class = "panel panel-primary",
-          div(class = "panel-heading", style = paste0("background-color:#ffffff"),
-              div(class = "row",
-                  fluidRow(    
-                    column(4),
-                    div(class = "col-xs-6", style = "align: center;",
-                        
+  valueBox_people1 <- function(imagen_vb, icon1, icon2, web1, web2, text, width_, height_,text2) {
+    div(class = "col-lg-3 col-md-11",
+        div(class = "panel panel-primary",
+            div(class = "panel-heading", style = paste0("background-color:#ffffff"),
+                div(class = "row",
+                    fluidRow(    
+                        column(4),
+                        div(class = "col-xs-6", style = "align: center;",
+                            
                         tags$a(tags$img(src=imagen_vb, height=width_,align = "center",width=height_,
                                         style="display: block; "),  href=web1)
                     ), 
                     div(class = ("col-xs-9 text-right"),
                         div(style = "font-size: 25px; color: #15202b;font-weight: bold; text-align:center; ",
-                            column(2),h4(text, style = "text-align:center; font-weight: bold;"), 
-                            column(2),h6(text2, style= "color: #7a7a7a; text-align:center")
+                            column(5),h4(text, style = "text-align:center; font-weight: bold;"), 
+                            column(5),h5(text2, style= "color: #7a7a7a; text-align:center"),
                         )
-                        
-                    ))
-              )),
-          div(class = "panel-footer",
-              div(class = "clearfix",
-                  div(class = "row",
-                      fluidRow(div(class = "col-xs-6",
-                                   column(4,tags$a(icon(icon1, "fa-2x"),href=web1))
-                      ),
-                      div(class = "col-xs-4",
-                          column(1,tags$a(icon(icon2, "fa-2x"),href=web2)))
-                      ))
-              ))
-      )
-  )
+                    
+                ))
+            )),
+            div(class = "panel-footer",
+                div(class = "clearfix",
+                    div(class = "row",
+                        fluidRow(div(class = "col-xs-6",
+                                     column(10),tags$a(icon(icon1, "fa-2x"),href=web1)
+                        ),
+                        div(class = "col-xs-4",
+                            column(1,tags$a(icon(icon2, "fa-2x"),href=web2)))
+                        ))
+                ))
+        )
+    )
+    
+  }
   
-}
+  valueBox_people2 <- function(imagen_vb, icon1, icon2, web1, web2, text, width_, height_,text2) {
+    div(class = "col-lg-3 col-md-11",
+        div(class = "panel panel-primary",
+            div(class = "panel-heading", style = paste0("background-color:#ffffff"),
+                div(class = "row",
+                    fluidRow(    
+                      column(4),
+                      div(class = "col-xs-6", style = "align: center;",
+                          
+                          tags$a(tags$img(src=imagen_vb, height=width_,align = "center",width=height_,
+                                          style="display: block; "),  href=web1)
+                      ), 
+                      div(class = ("col-xs-9 text-right"),
+                          div(style = "font-size: 25px; color: #15202b;font-weight: bold; text-align:center; ",
+                              column(5),h4(text, style = "text-align:center; font-weight: bold;"), 
+                              column(4),h5(text2, style= "color: #7a7a7a; text-align:center")
+                          )
+                          
+                      ))
+                )),
+            div(class = "panel-footer",
+                div(class = "clearfix",
+                    div(class = "row",
+                        fluidRow(div(class = "col-xs-6",
+                                     column(10),tags$a(icon(icon1, "fa-2x"),href=web1)
+                        ),
+                        div(class = "col-xs-4",
+                            column(1,tags$a(icon(icon2, "fa-2x"),href=web2)))
+                        ))
+                ))
+        )
+    )
+    
+  }
